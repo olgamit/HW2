630629c603f195d2133dbcf48962acd3bfab0423
Olga.Mityagina <Olga.Mityagina@sidenis.com>
2019-01-06 19:21:28 +0700

    Test row added

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("Hello," + args[2] + "!");
 
         System.out.println("Hello, World!");
+
+        System.out.println("Test row for new commit!");
     }
 }
