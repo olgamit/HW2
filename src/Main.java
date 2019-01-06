c16541507d78dc928f2837d383c6e82eaf14cb66
Olga.Mityagina <Olga.Mityagina@sidenis.com>
2019-01-06 19:12:45 +0700

    First Message

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String args[])
+    {
+
+        System.out.println("Hello," + args[0] + "!");
+
+        System.out.println("Hello," + args[1] + "!");
+
+        System.out.println("Hello," + args[2] + "!");
+
+    }
+}
